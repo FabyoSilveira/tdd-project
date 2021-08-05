3b5eef51aa107545c01eb56b315e354b94080600
Fabyo Silveira Amorim <silveira.fabyo@outlook.com>
2021-08-05 16:00:12 -0300

    Processando Requisição POST

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 
 class HomePageTest(TestCase):
 
-
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
@@ -16,4 +15,8 @@ class HomePageTest(TestCase):
         self.assertTrue(html.strip().endswith('</html>'))
 
         self.assertTemplateUsed(response, 'home.html') 
+    
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         
