e160e03be4d0ba403b01a2b87d862a186ed1b2d4
Fabyo Silveira Amorim <silveira.fabyo@outlook.com>
2021-08-09 21:30:02 -0300

    Implementando o Novo Projeto: Ajustando o Resto do Sistema

--- lists/tests.py
+++ lists/tests.py
@@ -30,9 +30,10 @@ class HomePageTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+    def test_displays_all_items(self):
+        my_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=my_list)
+        Item.objects.create(text='itemey 2', list=my_list)
 
         response = self.client.get('/')
 
